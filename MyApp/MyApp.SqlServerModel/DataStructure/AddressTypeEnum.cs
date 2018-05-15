0d834caa497fcc33e5f3dd99b481a8b81f4a2b88
mpiwko <marcpiwowarski@gmail.com>
2018-05-15 11:40:22 +0100

    added basic layout from bootstrap to the web project

---
+++ MyApp/MyApp.SqlServerModel/DataStructure/AddressTypeEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.SqlServerModel.DataStructure
+{
+    public enum AddressTypeEnum : byte
+    {
+        Type1,
+        Type2,
+        Type3
+    }
+}
