b473bac37757860abfc9ffa5631d3b3103561cb0
mpiwko <marcpiwowarski@gmail.com>
2018-05-15 13:14:24 +0100

    added frontend project, renamed MyApp to backend folder

---
+++ Frontend/src/app/index.route.js
@@ -0,0 +1,19 @@
+export function routerConfig ($stateProvider, $urlRouterProvider) {
+  'ngInject';
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'app/main/main.html',
+      controller: 'MainController',
+      controllerAs: 'main'
+    })
+    .state('about', {
+      url: '/',
+      templateUrl: 'app/about/about.html',
+      controller: 'AboutController',
+      controllerAs: 'about'
+    })
+    ;
+
+  $urlRouterProvider.otherwise('/');
+}
